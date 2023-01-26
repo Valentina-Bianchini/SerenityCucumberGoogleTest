e40020f1e0610e6fc95fd5592ccb73fd8d73fae7
valentina.bianchini <bianchinival19@gmail.com>
2023-01-26 16:23:40 -0300

    Sin error en el div

--- src/test/java/stepdefinitions/googleSteps.java
+++ src/test/java/stepdefinitions/googleSteps.java
@@ -49,7 +49,7 @@ public class googleSteps {
 
     @Then("^recibe resultados relacionados con la busqueda$")
     public void recibe_resultados_relacionados_con_la_busqueda(List<String> data) {
-        Target LINK = Target.the(" link a recursos relacionados a "+data.get(0)+"").locatedBy("//div[@id='result-stats añelhaefh']");
+        Target LINK = Target.the(" link a recursos relacionados a "+data.get(0)+"").locatedBy("//div[@id='result-stats']");
 
         actor.attemptsTo(WaitUntil.the(LINK, isVisible()).forNoMoreThan(30).seconds());
 
